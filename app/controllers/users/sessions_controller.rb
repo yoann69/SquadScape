1b8dc83d412bef6a24c65f80d839f957797e1b4c
yoann morillo <yoann.morillo@hotmail.fr>
2023-08-09 14:24:28 +0200

    connected ok

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   # Redirige l'utilisateur vers la page principale après une connexion réussie.
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     root_path
   end
-  def after_sign_up_path_for(resource)
-    root_path
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'dashboard/index'
   root 'pages#home'
   devise_for :users, controllers: { sessions: 'users/sessions' }
-
   get 'dashboard', to: 'dashboard#index'
 end
